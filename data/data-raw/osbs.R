fe048e42339b5d8bb8bca38e3b41d8430b4d2384
Jeff <jwatkins6@vcu.edu>
2017-08-15 16:44:32 -0400

    updating

--- data/data-raw/osbs.R
+++
@@ -1,9 +0,0 @@
-
-# Imports the raw data from Ordway-Swisher Biological Station in Florida, US.
- osbs <- read.csv("./data-raw/osbs.CSV", header=FALSE, col.names = c("return_distance", "intensity"), blank.lines.skip = FALSE)
-
-# Adds index values from row names and reorganizes data frame.
- osbs$index <- as.numeric(rownames(osbs))
- osbs = osbs[,c(3, 1, 2)]
-
- devtools:::use_data(osbs,  overwrite = TRUE, pkg = "./data-raw")
