83f554a6060f05f82e6644b10a80604dc0a8371f
Jeff <jwatkins6@vcu.edu>
2017-11-16 12:33:40 -0500

    playing with stdbin

--- R/calc_tls_csc.R
+++ R/calc_tls_csc.R
@@ -35,13 +35,13 @@ calc_tls_csc <- function(m, filename){
 
   super.size[is.na(super.size)] <- 0
   #print(super.size)
-  std.std = sum(super.size$std.bin.squared) / transect.length
+  std.std = mean(super.size$std.bin.squared) / transect.length
   # std.std = ((mean(super.size$std.bin^2))) / transect.length
 
   # mean.std = mean(super.size$std.bin)
-  mean.std = sum(super.size$std.bin) / transect.length
+  mean.std = mean(super.size$std.bin) / transect.length
 
-
+  print(super.size)
 
   # HEIGHT VARIABLES
   message("HEIGHT METRICS")
